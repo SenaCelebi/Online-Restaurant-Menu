8b5589bf2cd02981b74f8d3a7c2fe346294b5420
Melike Doğru <dogrumelike98@gmail.com>
2020-04-25 17:21:28 +0300

    customer page

---
+++ src/constants.js
@@ -0,0 +1,104 @@
+export const dessertList = [
+  {
+    title: "Air Fryer Donuts",
+    price: "$100",
+    description:
+      "Skip the pot of hot oil and make these healthier air fryer donuts. If you've ever had a baked donut, brace yourself. These are SO much better.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190619-air-fryer-donuts-305-portrait-pf-1561758031.jpg?crop=0.9997369113391213xw:1xh;center,top&resize=980:* "
+  },
+  {
+    title: "Air Fryer S'mores",
+    price: "$80",
+    description:
+      "Put the bug spray down, your new favorite s'more can be made without a campfire.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190731-air-fryer-smores-0269-portrait-pf-1565820637.jpg?crop=1xw:1xh;center,top&resize=980:*"
+  },
+  {
+    title: "Air Fryer Molten Lava Cake",
+    price: "$110",
+    description:
+      "It's Molten! It's Molteeennn! Yes, this molten lava cake is Chef approved.Lorem ipsum dolor sit amet.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/molten-air-fryer-l-1562037316.png?crop=0.936xw:1.00xh;0,0&resize=980:*"
+  },
+  {
+    title: "Crustless Air Fryer Cheesecake",
+    price: "$100",
+    description:
+      "Is there anything an Air Fryer CAN'T do!? Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    imageUrl:
+      "https://www.recipethis.com/wp-content/uploads/Best-Ever-Philips-Airfryer-Caramel-Cheesecake.jpg"
+  },
+];
+
+export const soapList = [
+  {
+    title: "Pasta Fagioli",
+    price: "$149.99",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/del.h-cdn.co/assets/17/31/1600x2399/gallery-1501791386-delish-pasta-fagioli-vertical.jpg?resize=980:*"
+  },
+  {
+    title: "Tuscan Tortellini Soup",
+    price: "$330",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/del.h-cdn.co/assets/16/39/1474916963-delish-tortellini-soup-1.jpg?crop=1xw:0.99975xh;center,top&resize=980:*"
+  },
+  {
+    title: "Chicken Parm Soup",
+    price: "$90",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/del.h-cdn.co/assets/15/40/1443717944-chicken-parm-soup-delish.jpg?crop=1.0xw:1xh;center,top&resize=980:*"
+  },
+  {
+    title: "Pizza Soup",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/del.h-cdn.co/assets/16/39/2048x3071/gallery-1474914960-delish-pizza-soup.jpg?resize=980:*"
+  },
+];
+
+export const mealList = [
+  {
+    title: "Chicken Marsala",
+    price: "$200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190814-chicken-marsala-0108-portrait-pf-1567185387.jpg?crop=1xw:1xh;center,top&resize=980:*"
+  },
+  {
+    title: "Air Fryer Pizza",
+    price: "$300",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190731-air-fryer-pizza-0188-portrait-pf-1565820597.jpg?crop=0.9997369113391213xw:1xh;center,top&resize=980:*"
+  },
+  {
+    title: "Cumin Spiced Chicken Meatballs",
+    price: "$90",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-191907-chicken-meatballs-0323-portrait-1565386516.jpg?crop=0.9997369113391213xw:1xh;center,top&resize=980:*"
+  },
+  {
+    title: "Pasta Puttanesca",
+    price: "$100",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium diam orci, a ultrices enim tincidunt nec.",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/singleimagebug-pasta-puttanesca-pin-1549421288.jpg?crop=1xw:0.9453781512605042xh;center,top&resize=980:*"
+  },
+];
